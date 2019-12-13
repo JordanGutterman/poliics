2a1be5097755c7958251a7add1a40572cdde41ed
Jordan Gutterman <jordan.gutterman@gmail.com>
2019-12-12 22:52:08 -0600

    Add two more models and the SNP

--- UK_election.py
+++ UK_election.py
@@ -3,7 +3,8 @@
 # simple model built from announced results
 import pandas as pd
 import sklearn
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+pd.options.mode.chained_assignment = None  # default='warn'
 
 # working data file, plus mrp
 live = pd.read_excel('data/mrp_and_live.xlsx')
@@ -39,17 +40,20 @@ brexit = brexit.dropna()
 data = pd.merge(data, brexit, on = 'Constituency', how = 'outer')
 
 # and live/mrp, dropping non-main parties
-live = live.drop(['Brexit_MRP', 'Green_MRP', 'SNP_MRP', 'PC_MRP', 'Other_MRP'], 1)
+live = live.drop(['Brexit_MRP', 'Green_MRP', 'PC_MRP', 'Other_MRP'], 1)
 data = pd.merge(data, live, on = 'Constituency', how = 'outer')
 data = data.drop(['Constituency', 'ONS ID', 'code'], 1)
 
 # Create and spilt training set
 train_set = data.dropna(subset=['Con_Live'])
+train_set = train_set.dropna()
 train_coeffs = train_set.drop([col for col in train_set if col.endswith('_Live')], axis=1)
 train_outcomes = train_set[[col for col in train_set if col.endswith('_Live')]]
 
-# train model with results so far
+# train models with results so far
 lr = LinearRegression().fit(train_coeffs, train_outcomes)
+rlr = Ridge().fit(train_coeffs, train_outcomes)
+llr = Lasso().fit(train_coeffs, train_outcomes)
 
 # Create prediction set
 pred_set = data[data['Con_Live'].isnull()]
@@ -58,15 +62,26 @@ pred_set = pred_set.drop([col for col in train_set if col.endswith('_Live')], ax
 # drop all our NAs
 pred_set = pred_set.dropna()
 
+def count_winners(df) :
+    df['C_win'] = (df['Con_Live'] > df['Lab_Live']) & (df['Con_Live'] > df['LD_Live']) & (df['Con_Live'] > df['SNP_Live'])
+    df['Lab_win'] = (df['Lab_Live'] > df['Con_Live']) & (df['Lab_Live'] > df['LD_Live']) & (df['Lab_Live'] > df['SNP_Live'])
+    df['LD_win'] = (df['LD_Live'] > df['Con_Live']) & (df['LD_Live'] > df['Lab_Live']) & (df['LD_Live'] > df['SNP_Live'])
+    df['SNP_win'] = (df['SNP_Live'] > df['Con_Live']) & (df['SNP_Live'] > df['Lab_Live']) & (df['SNP_Live'] > df['LD_Live'])
+    return df
+
+train_set = count_winners(train_set)
+
 # run predict
-pred_outcomes = pd.DataFrame(lr.predict(pred_set))
-pred_outcomes.columns = train_outcomes.columns
+def run_pred(model, pred_set) :
+    pred_outcomes = pd.DataFrame(model.predict(pred_set))
+    pred_outcomes.columns = train_outcomes.columns
 
-# count predicted winners
-pred_outcomes['C_win'] = (pred_outcomes['Con_Live'] > pred_outcomes['Lab_Live']) & (pred_outcomes['Con_Live'] > pred_outcomes['LD_Live'])
-pred_outcomes['Lab_win'] = (pred_outcomes['Lab_Live'] > pred_outcomes['Con_Live']) & (pred_outcomes['Lab_Live'] > pred_outcomes['LD_Live'])
-pred_outcomes['LD_win'] = (pred_outcomes['LD_Live'] > pred_outcomes['Con_Live']) & (pred_outcomes['LD_Live'] > pred_outcomes['Lab_Live'])
+    return pred_outcomes
 
-sum(pred_outcomes['C_win'])
-sum(pred_outcomes['Lab_win'])
-sum(pred_outcomes['LD_win'])
+pred_outcomes = count_winners(run_pred(lr, pred_set))
+pred_outcomesR = count_winners(run_pred(rlr, pred_set))
+pred_outcomesL = count_winners(run_pred(llr, pred_set))
+
+print('C:' + str(sum(pred_outcomes['C_win']) + sum(train_set['C_win'])) + ' L:' + str(sum(pred_outcomes['Lab_win']) + sum(train_set['Lab_win'])) + ' LD:' + str(sum(pred_outcomes['LD_win']) + sum(train_set['LD_win'])) + ' SNP:' + str(sum(pred_outcomes['SNP_win']) + sum(train_set['SNP_win'])))
+print('C:' + str(sum(pred_outcomesR['C_win']) + sum(train_set['C_win'])) + ' L:' + str(sum(pred_outcomesR['Lab_win']) + sum(train_set['Lab_win'])) + ' LD:' + str(sum(pred_outcomesR['LD_win']) + sum(train_set['LD_win'])) + ' SNP:' + str(sum(pred_outcomes['SNP_win']) + sum(train_set['SNP_win'])))
+print('C:' + str(sum(pred_outcomesL['C_win']) + sum(train_set['C_win'])) + ' L:' + str(sum(pred_outcomesL['Lab_win']) + sum(train_set['Lab_win'])) + ' LD:' + str(sum(pred_outcomesL['LD_win']) + sum(train_set['LD_win'])) + ' SNP:' + str(sum(pred_outcomes['SNP_win']) + sum(train_set['SNP_win'])))
